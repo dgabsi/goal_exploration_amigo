1aa07f190d05f19cdb7c886cd32ee72e859b2c4a
neuralx <daniela@neuralx.co.uk>
2021-10-01 15:52:00 +0100

    add csv files

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ parser.add_argument("--log-interval", type=int, default=1,
                     help="number of updates between two logs (default: 1)")
 parser.add_argument("--save-interval", type=int, default=10,
                     help="number of updates between two saves (default: 10, 0 means no saving)")
-parser.add_argument("--procs", type=int, default=16,
+parser.add_argument("--procs", type=int, default=40,
                     help="number of processes (default: 16)")
 parser.add_argument("--frames", type=int, default=10**7,
                     help="number of frames of training (default: 1e7)")
